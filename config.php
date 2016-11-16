11ba1e2f20f7df4503b022f19981ead7db213a78
Brian McMichael <bmcmichael@osc.edu>
2016-11-16 12:22:23 -0500

    add accessors for email and website

--- config.php
+++ config.php
@@ -6,6 +6,8 @@ class Config {
   public $nav_border_color = '#000';
   public $nav_brand_text_color = '#fff';
   public $change_password_url = null;
+  public $help_email = '';
+  public $help_website = '';
 }
 
 $config = new Config;
@@ -15,10 +17,14 @@ $config->title = 'OSC OnDemand';
 $config->nav_background_color = '#cf102d';
 $config->nav_border_color = '#a00c23';
 $config->change_password_url = 'https://my.osc.edu';
+$config->help_email = 'oschelp@osc.edu';
+$config->help_website = 'https://www.osc.edu/content/email_osc';
 
 /* Minnesota Supercomputing Center Example */
 /* $config->title = 'MSI OnDemand'; */
 /* $config->nav_background_color = '#ffd75f'; */
 /* $config->nav_border_color = '#000'; */
 /* $config->nav_brand_text_color = '#000'; */
+/* $config->help_email = 'help@msi.umn.edu'; */
+/* $config->help_website = 'https://www.msi.umn.edu/support/request'; */
 ?>
